872d8c321eaa3029748ed967621c1553b033ca88
rachaelog <r.b.ogungbamigbe@gmail.com>
2022-04-09 15:31:37 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@ $connection = new mysqli ('localhost', 'root', '');
 if (!$connection){
   die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'register');
+$select_db = mysqli_select_db($connection, 'test');
 if (!$select_db){
   die("Database Selection Failed" . mysqli_error($connection));
   }
