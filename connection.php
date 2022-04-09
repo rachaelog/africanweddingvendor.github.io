416ec6eb0194f73deba634cce96e4f8c0c8881cd
rachaelog <r.b.ogungbamigbe@gmail.com>
2022-04-09 16:16:07 +0100

    new
    new

--- connection.php
+++ connection.php
@@ -1,14 +1,43 @@
 <?php
+$name = filter_input(INPUT_POST, 'name');
+$email = filter_input(INPUT_POST, 'email');
+$message = filter_input(INPUT_POST, 'message');
 
-$sname = "localhost";
-$uname = "root";
-$password = "";
+if (!empty($name)){
+if (!empty($email))
+if (!empty($message)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "test";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 
-$db_name = "test";
 
-$conn = mysqli_connect($sname, $uname, $password, $db_name);
-
-if (!$conn) {
-	echo "Connection failed!";
-	exit();
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
 }
+else{
+$sql = "INSERT INTO register (name, email, message)
+values ('$name','$email','$message')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+?>
