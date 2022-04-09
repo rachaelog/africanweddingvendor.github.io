f61bfb5d17dd8e1492f5a28310279ddff9f334f1
rachaelog <r.b.ogungbamigbe@gmail.com>
2022-04-09 14:18:11 +0100

    submit button active
    created php submit code

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+	$Name = $_POST['Name'];
+	$email = $_POST['Email'];
+	$message = $_POST['Message'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(Name, Email, Message) values(?, ?, ?)");
+		$stmt->bind_param("sssssi", $Name, $email, $message);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
